2867eb761d632499daf2c827c348bab8493dba6e
Shubham Kaushal <kaushalshubham460@gmail.com>
2024-07-25 13:18:48 +0530

    resolve

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import adminLoginApi from "./api/AdminLogin";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
