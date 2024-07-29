fe22e5ec3d45471d0d9803bfa143cac1c45b0872
Asif Ali <asifkhan3596073@gmail.com>
2024-07-29 15:38:32 +0530

    started dashboard content

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import DashboardComp from "../src/components/Dashboard/Dashboard.jsx";
 import Login from "./components/LoginPage/LoginPage.jsx";
 import { OpenNotification } from "./components/Snackbar.jsx";
+import DashboardComponent from "../src/components/Dashboard/Dashboard.jsx";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<DashboardComp />} />
+        <Route path="/dashboard" element={<DashboardComponent />} />
       </Routes>
       <OpenNotification />
     </div>
---
+++ src/components/DashboardContent/dashboardContentStyled.js
@@ -0,0 +1,37 @@
+import { Box, Button, Paper, styled, Typography } from "@mui/material";
+
+
+export const DashboardMainContainer = styled(Box)(() => ({
+  width: "100%",
+  height: "500px",
+  backgroundColor:"red"
+
+}));
+export const DashboardCardWrapper = styled(Box)(() => ({
+  width: "100%",
+  height: "170px",
+  backgroundColor:"green",
+  display:"flex",
+  flexDirection:"column",
+  justifyContent:"space-between"
+
+}));
+export const DashboardHeading = styled(Typography)(() => ({
+  color: "#262626 !important",
+  fontSize: "16px",
+  fontWeight:"600",
+  backgroundColor:"yellow"
+
+}));
+export const DashboardCardBox = styled(Typography)(() => ({
+  width:"100%",
+  height:"134px",
+  backgroundColor:"orange"
+
+}));
+export const DashboardCard = styled(Typography)(() => ({
+  width:"30%",
+  height:"inherit",
+  backgroundColor:"white"
+
+}));
