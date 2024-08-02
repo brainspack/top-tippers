324cc6635d5e3c1780b42afefbdb701397ebf4ab
Asif Ali <asifkhan3596073@gmail.com>
2024-08-02 09:44:25 +0530

    commit resolve

--- src/store.js
+++ src/store.js
@@ -18,8 +18,6 @@ export const store = configureStore({
     [deactivateUserApi.reducerPath]: deactivateUserApi.reducer,
     [userListCompetitionApi.reducerPath]: userListCompetitionApi.reducer,
     [userListSportApi.reducerPath]: userListSportApi.reducer,
-    [deleteUserApi.reducerPath]: deleteUserApi.reducer,
-    [userDetailsApi.reducerPath]: userDetailsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
@@ -30,8 +28,6 @@ export const store = configureStore({
       userListSportApi.middleware
       
 
-      deleteUserApi.middleware,
-      userDetailsApi.middleware
     ),
 });
 setupListeners(store.dispatch);
